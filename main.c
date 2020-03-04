c3ea4ac8e494a9fd372abc5cd0b3582afee551c7
Enzo Dal Cerro <enzodalcerro@gmail.com>
2020-03-04 17:09:15 +0100

    strlen

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
-extern void start();
+extern int strlen(char *c);
 int main()
 {
-	start();
+	int len = strlen("hello");
+	printf("len = %d\n",len );
 	return 0;
 }
 
@@ -9,3 +10,8 @@ int main()
 //TORUN 
 //nasm -fmacho64 hello.s
 //gcc hello.o main.c
+
+
+// nasm -f elf64 hello.s
+//ld -s -o hello hello.o
+//
